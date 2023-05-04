c790bfb79bed1bc3fe4178082c7791ec8333affd
Sergiyan92 <sergiyan@ukr.net>
2023-05-04 14:06:29 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,6 +4,10 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const inputDatatime = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
+const dayTime = document.querySelector('[data-days]');
+const hoursTime = document.querySelector('[data-hours]');
+const minutesTime = document.querySelector('[data-minutes]');
+const secondsTime = document.querySelector('[data-seconds]');
 
 const options = {
   enableTime: true,
@@ -18,16 +22,41 @@ const options = {
       return;
     }
     startBtn.disabled = false;
+
+    startBtn.addEventListener('click', () => {
+      const timerInterval = setInterval(() => {
+        const currentTime = new Date().getTime();
+        const timeLeft = selectedDate.getTime() - currentTime; //
+        console.log(timeLeft);
+        function convertMs(ms) {
+          // Number of milliseconds per unit of time
+          const second = 1000;
+          const minute = second * 60;
+          const hour = minute * 60;
+          const day = hour * 24;
+          // Remaining days
+          const days = Math.floor(ms / day);
+          // Remaining hours
+          const hours = Math.floor((ms % day) / hour);
+          // Remaining minutes
+          const minutes = Math.floor(((ms % day) % hour) / minute);
+          // Remaining seconds
+          const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+          dayTime.textContent = `${days}`;
+          hoursTime.textContent = `${hours}`;
+          minutesTime.textContent = `${minutes}`;
+          secondsTime.textContent = `${seconds}`;
+          if (timeLeft < 0) {
+            clearInterval(timerInterval);
+            dayTime.textContent = 00;
+            hoursTime.textContent = 00;
+            minutesTime.textContent = 00;
+            secondsTime.textContent = 00;
+          }
+        }
+        console.log(convertMs(timeLeft));
+      }, 1000);
+    });
   },
 };
 flatpickr(inputDatatime, options);
-
-startBtn.addEventListener('click', e => {
-  const selectedDate = flatpickr.parseDate(inputDatatime.value);
-
-  intervalId = setInterval(function () {
-    const timeDiff = selectedDate.getTime() - Date.now();
-    const secondsDiff = Math.floor(timeDiff / 1000);
-    console.log(`${secondsDiff} seconds left`);
-  }, 1000);
-});
